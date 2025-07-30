10e8b6b051b6296f2f7f1e5d0db981ce24e801b8
DESKTOP-2BD7O9E\vohun <vohung.it@gmail.com>
2025-07-31 00:24:21 +0700

    update BE

--- src/modules/folk-medicine.module.ts
+++ src/modules/folk-medicine.module.ts
@@ -5,13 +5,17 @@ import { User } from '../entities/user.entity';
 import { Category } from '../entities/category.entity';
 import { FolkMedicineService } from '../services/folk-medicine.service';
 import { FolkMedicineController } from '../controllers/folk-medicine.controller';
+import { Permission } from 'src/entities/permission.entity';
+import { Feature } from 'src/entities/feature.entity';
+import { RoleService } from 'src/services/role.service';
+import { Role } from 'src/entities/role.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([FolkMedicine, User, Category]),
+    TypeOrmModule.forFeature([FolkMedicine,Role, User, Category, Permission, Feature]),
   ],
   controllers: [FolkMedicineController],
-  providers: [FolkMedicineService],
+  providers: [FolkMedicineService, RoleService],
   exports: [FolkMedicineService],
 })
 export class FolkMedicineModule {} 
