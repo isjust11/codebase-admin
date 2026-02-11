31ae41017b6c7ce85a84e5366b7ac7ca5cb5a3b1
Võ Văn Hùng <vohung.it2@gmail.com>
2026-02-11 17:37:31 +0700

    restore

--- src/app.module.ts
+++ src/app.module.ts
@@ -79,7 +79,7 @@ import { UserSubscription } from './entities/user-subscription.entity';
         SubscriptionPlan,
         UserSubscription,
       ],
-      synchronize: false,
+      synchronize: true,
       migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
       migrationsRun: true,
       // Cấu hình để hỗ trợ MySQL 8.0+ với caching_sha2_password
