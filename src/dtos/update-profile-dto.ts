f6b7305654b6ce6d63b6dee8266fb17b06d67cf4
Võ Văn Hùng <vohung.it2@gmail.com>
2026-03-21 21:00:25 +0700

    Remove `userId` field and `IsNotEmpty` decorator from multiple fields in `UpdateProfileDto`.

--- src/dtos/update-profile-dto.ts
+++ src/dtos/update-profile-dto.ts
@@ -1,17 +1,11 @@
 import { IsNotEmpty, IsString, Matches } from "class-validator";
 
 export class UpdateProfileDto {
-  @IsString()
-  @IsNotEmpty()
-  userId: string;
 
   @IsString()
-  @IsString()
-  @IsNotEmpty()
   fullName: string;
 
   @IsString()
-  @IsNotEmpty()
   picture: string;
 
   @IsString()
@@ -23,29 +17,23 @@ export class UpdateProfileDto {
   phoneNumber: string;
 
   @IsString()
-  @IsNotEmpty()
   address: string;
 
   @IsString()
-  @IsNotEmpty()
   @Matches(/^\d{2}\/\d{2}\/\d{4}$/, {
     message: 'birthDate must be in format dd/MM/yyyy'
   })
   birthDate: string;
 
   @IsString()
-  @IsNotEmpty()
   facebookLink: string;
 
   @IsString()
-  @IsNotEmpty()
   instagramLink: string;
 
   @IsString()
-  @IsNotEmpty()
   twitterLink: string;
 
   @IsString()
-  @IsNotEmpty()
   linkedinLink: string;
 }
