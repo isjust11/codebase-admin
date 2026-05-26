1e22f990e2abde0e3bfd0671e6138aebe194242b
Võ Văn Hùng <vohung.it2@gmail.com>
2026-05-26 22:46:44 +0700

    add policy, support file

--- src/main.ts
+++ src/main.ts
@@ -57,7 +57,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
-
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   // Swagger - chỉ bật khi development (tránh lộ API trên production)
   if (process.env.NODE_ENV !== 'production') {
     const config = new DocumentBuilder()
